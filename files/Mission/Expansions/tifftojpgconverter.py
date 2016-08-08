146206bb206ff2463f4075717cdc9301ec7a3f79
Kevin Chang <kckaiwei@gmail.com>
2016-08-08 10:34:43 -0500

    converted rest of .tiff into no whitespace format

--- files/Mission/Expansions/tifftojpgconverter.py
+++ files/Mission/Expansions/tifftojpgconverter.py
@@ -4,13 +4,11 @@ import glob
 for name in glob.glob('*.tif'):
     im = Image.open(name)
     name = str(name).rstrip(".tif")
-    name = name.replace(" ","")
     im.save(name + '.jpg', 'JPEG')
 
 for name in glob.glob('*.tiff'):
     im = Image.open(name)
     name = str(name).rstrip(".tiff")
-    name = name.replace(" ","")
     im.save(name + '.jpg', 'JPEG')
 
 print "Conversion from tif/tiff to jpg completed!"
